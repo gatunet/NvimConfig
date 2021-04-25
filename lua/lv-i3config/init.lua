ad57d36fe43c3aff3d80d6bec45e66816bd95237
gartunius <gabriel.ataidesantos@gmail.com>
2021-04-25 14:36:15 -0300

    Deleting entire config

--- lua/lv-i3config/init.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd([[
-aug i3config_ft_detection
-  au!
-  au BufNewFile,BufRead ~/.config/i3/config set filetype=i3config
-  au BufNewFile,BufRead ~/.config/sway/config set filetype=i3config
-aug end
-]])

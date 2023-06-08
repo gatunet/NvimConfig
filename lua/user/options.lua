1c42e76783b8b6e962a492152af0baeb66dd3900
Gabriel <gabriel@Gabriels-MacBook-Pro.local>
2023-06-08 17:16:56 -0300

    Changing background to transparent

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -54,7 +54,7 @@ end
 -- Sonokai
 vim.g.sonokai_style = "shusia"
 vim.g.sonokai_enable_italic = 1
-vim.g.sonokai_transparent_background = 0
+vim.g.sonokai_transparent_background = 1
 
 vim.cmd([[colorscheme sonokai]])
 
